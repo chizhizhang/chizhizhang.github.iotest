b3d6453357cb6eef4fcaf575c17d71c643031365
Chizhi Zhang <chizhizhang@hotmail.com>
2024-04-20 21:44:50 +0800

    1

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-document.getElementById('dark-mode-toggle').addEventListener('click', function() {
-    const hasDarkMode = document.body.classList.contains('dark-mode');
-    const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    
-    if (hasDarkMode || (!hasDarkMode && !systemPrefersDark)) {
-        document.body.classList.remove('dark-mode');
-        // Save the user's preference in local storage or cookies
+document.querySelector('.search-toggle').addEventListener('click', function() {
+    var searchBox = document.getElementById('search-box');
+    if (searchBox.style.display === 'block') {
+        searchBox.style.display = 'none';
     } else {
-        document.body.classList.add('dark-mode');
-        // Save the user's preference in local storage or cookies
+        searchBox.style.display = 'block';
     }
 });
+
+
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+// Check the user's system preferred color scheme
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    // If the system is in dark mode, select the 'Dark' option
+    darkModeToggle.value = 'dark';
+} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+    // If the system is in light mode, select the 'Light' option
+    darkModeToggle.value = 'light';
+} else {
+    // If the system preferred color mode cannot be detected, you can choose a default option, such as 'Auto'
+    darkModeToggle.value = 'auto';
+}
+
